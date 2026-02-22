54d141901869b81efa5c5a4c3e6fc02e5b65e9fc
kot <kostia.shiian@gmail.com>
2026-02-22 16:29:24 +0200

    chore(infra): remove Route53 configuration and outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -9,17 +9,4 @@ output "cd_user_access_key_secret" {
   sensitive   = true
 }
 
-##########################
-# Route53 Outputs        #
-##########################
-
-output "route53_zone_id" {
-  description = "Route53 hosted zone ID"
-  value       = aws_route53_zone.main.zone_id
-}
-
-output "route53_zone_name" {
-  description = "Route53 hosted zone name"
-  value       = aws_route53_zone.main.name
-}
 
