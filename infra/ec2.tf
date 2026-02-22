34347611776b334a19f7d5618a53310716b4c3f8
kot <kostia.shiian@gmail.com>
2026-02-22 20:05:27 +0200

    fix: update HTML header in user data script for EC2 instance

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "web" {
     dnf install -y nginx
     systemctl enable nginx
     systemctl start nginx
-    echo "<h1>Hello World</h1>" > /usr/share/nginx/html/index.html
+    echo "<h1>Hello World!</h1>" > /usr/share/nginx/html/index.html
   EOF
 
   tags = {
