e091fc9c80c2878648449ab22cc482c1afb12184
kot <kostia.shiian@gmail.com>
2026-02-22 19:19:45 +0200

    Add ec2

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "web" {
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public_a.id
   vpc_security_group_ids = [aws_security_group.web.id]
 
