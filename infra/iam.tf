6be317a41ad51aa46a7c6097f722fdc977e2a5a3
kot <kostia.shiian@gmail.com>
2026-02-22 16:20:19 +0200

    chore(iam): simplify S3 backend policy and add admin access

--- infra/iam.tf
+++ infra/iam.tf
@@ -17,8 +17,7 @@ data "aws_iam_policy_document" "tf_s3_backend" {
     effect  = "Allow"
     actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
     resources = [
-      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
-      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-setup"
     ]
   }
 }
@@ -60,4 +59,9 @@ resource "aws_iam_user_policy_attachment" "tf_s3_backend" {
 resource "aws_iam_user_policy_attachment" "tf_dynamodb_backend" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.tf_dynamodb_backend.arn
+}
+
+resource "aws_iam_user_policy_attachment" "admin_access" {
+  user       = aws_iam_user.cd.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
