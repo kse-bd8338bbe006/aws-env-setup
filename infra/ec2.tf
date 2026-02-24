1d9ff5301d36c924c6fe1c1ecf67ca64852dbc08
kot <kostia.shiian@gmail.com>
2026-02-24 21:27:16 +0200

    fix(infra): update nginx welcome message

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -100,7 +100,7 @@ resource "aws_instance" "web" {
     dnf install -y nginx
     systemctl enable nginx
     systemctl start nginx
-    echo "<h1>Hello World!</h1>" > /usr/share/nginx/html/index.html
+    echo "<h1>Hello World again!</h1>" > /usr/share/nginx/html/index.html
   EOF
 
   tags = {
